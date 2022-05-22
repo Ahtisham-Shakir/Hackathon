973088069de0fe98c8ad46dd76b705910d04cf8a
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-05-22 13:07:51 +0500

    Added navbar and main banner

---
+++ e-commerce-website/src/components/banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import mainBanner from '../../assets/main-banner.jpg'
+import { Link } from 'react-router-dom'
+
+const Banner = () => {
+    return (
+        <div id="banner" style={{ backgroundImage: `url(${mainBanner})`, height : "75vh"  }}>
+            <div id="layer" style={{ background: `rgba(0,0,0,0.4)` }} className="h-100 py-5">
+            <div className="container text-white text-center h-100 py-5 ">
+                <h1 className='text-white text-center'>Welcome to Cartify</h1>
+                <p className='px-5 my-3 fs-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem qui tempora odio officia, aperiam amet mollitia nesciunt quas quisquam voluptatum consequatur vitae earum? Assumenda beatae officiis soluta doloremque animi, quisquam accusamus quo iure eveniet optio adipisci labore. Aperiam, corporis iure? Unde sapiente facere voluptate provident labore omnis magni ex in?
+                </p>
+                <button className='btn btn-primary'><Link to='/' className='text-white text-decoration-none'>SHOP NOW</Link></button>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
--- e-commerce-website/src/components/navbar/Navbar.jsx
+++ e-commerce-website/src/components/navbar/Navbar.jsx
@@ -1,44 +1,45 @@
 import React from 'react'
+import './Navbar.styles.css'
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/logo.png'
+import CartIcon from '../../assets/cart-icon.svg'
 
 const Navbar = () => {
   return (
+
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+      <div className="container">
+        <Link className="navbar-brand image-fluid" to="/"><img src={Logo} alt="" id='logo'/></Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+            <li className="nav-item mx-2" >
+              <Link className="nav-link text-dark" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item mx-2" >
+              <Link className="nav-link text-dark" to="/shop">Shop</Link>
+            </li>
+            <li className="nav-item mx-2" >
+              <Link className="nav-link text-dark" to="/signin">SignIn</Link>
+            </li>
+            <li className="nav-item mx-2" >
+              <Link className="nav-link text-dark" to="/signin"></Link>
+            </li>
           </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-primary" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+
+
+
+
   )
 }
 
-export default Navbar.component
+export default Navbar
